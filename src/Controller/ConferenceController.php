72aeec74fdb2e0b6468e531c37b83b9a49d29a5c
Alex Dontsov <dcorpsoftware@gmail.com>
2020-07-22 10:06:51 +0700

    render

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,10 +3,30 @@
 namespace App\Controller;
 
 use App\Entity\Conference;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class ConferenceController extends AbstractController
 {
-
+    /**
+     * @Route("/", name="homepage")
+     * @param Environment $twig
+     * @param ConferenceRepository $conferenceRepository
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository)
+    {
+        return new Response($twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
+    }
 }
